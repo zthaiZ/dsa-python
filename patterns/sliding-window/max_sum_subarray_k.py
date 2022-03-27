148257991ab98807b65a54a103f097990b37158a
Steven <thai2@umbc.edu>
2022-03-26 20:35:20 -0400

    rename first tree example, add reverse order traversal example

--- patterns/sliding-window/max_sum_subarray_k.py
+++ patterns/sliding-window/max_sum_subarray_k.py
@@ -3,9 +3,11 @@
 def max_sub_array_of_size_k(arr, k):
   start = 0
   window_sum = 0
-  max_sum = 0
+  max_sum = 
+  
   for end in range(len(arr)):
     window_sum += arr[end]
+    
     if end >= k-1:
       max_sum = max(max_sum, window_sum)
       window_sum -= arr[start]
