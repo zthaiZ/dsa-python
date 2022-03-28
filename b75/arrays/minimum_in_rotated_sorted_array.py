7ca851ee93e38ab5dad1da0e31d58e43ef2ac34f
Steven <thai2@umbc.edu>
2022-03-28 00:56:20 -0400

    add example for minimum in rotated sorted array

---
+++ b75/arrays/minimum_in_rotated_sorted_array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        # utilize binary search
+        # array is basically half sorted, find the portion that has the lower end of the sort
+        # once we know the sorted portion with smaller numbers, grab that leftmost value
+        
+        left = 0
+        right = len(nums)-1
+        res = nums[0]
+        
+        while (left < right):
+            if nums[left] < nums[right]:
+                res = min(res, nums[left])
+                break
+            mid = (right-left) // 2
+            res = min(res, nums[mid])
+            
+            if nums[left] <= nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
+                
+        return res
+        
