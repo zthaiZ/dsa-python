c35097791a9439d307df3eecd80fdc71557edee4
Steven <thai2@umbc.edu>
2022-04-02 21:45:04 -0400

    added missing return statement

--- b75/interval/insert_interval.py
+++ b75/interval/insert_interval.py
@@ -11,4 +11,5 @@ class Solution:
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
                 
+        res.append(newInterval)
         return res
